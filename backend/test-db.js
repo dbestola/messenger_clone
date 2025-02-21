3fc98f35f21539c291afa2a8c4b17d40eb9add7a
OLANREWAJU Oladimeji Nathaniel <olanrewajuoladimeji5@gmail.com>
2025-02-21 22:59:04 +0100

    updates...

--- backend/test-db.js
+++ backend/test-db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://olanrewajuoladimeji5:KVUoWJoASKFRwKLN@cluster0.btemb6a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI, {
   serverSelectionTimeoutMS: 10000, // Set timeout to 10s
