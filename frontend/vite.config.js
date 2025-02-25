2d75b6828f7f63308decfd2daa326e68f9752f87
OLANREWAJU Oladimeji Nathaniel <olanrewajuoladimeji5@gmail.com>
2025-02-25 07:48:24 +0100

    updates...

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
   server: {
-    historyApiFallback: true,
+    proxy: {
+      "/api": "http://localhost:5000", // Adjust this if using an API
+    },
   },
 });
