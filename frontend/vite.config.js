64f2181cac42f05c1ae46501b6ca176e3cd86479
OLANREWAJU Oladimeji Nathaniel <olanrewajuoladimeji5@gmail.com>
2025-02-25 07:41:58 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Ensure correct base URL
   server: {
-    historyApiFallback: true, // Enable history fallback
-  }
+    historyApiFallback: true,
+  },
 });
